e2ec3bfa6a1c318242d599f1cac7fca26224af79
Alexandre Jamet <themisterkiwiii@gmail.com>
2013-07-25 07:50:27 -0700

    Linkage correction and adding a nez assmbly function.

--- src/core/hardware/CPUInfo.cpp
+++ src/core/hardware/CPUInfo.cpp
@@ -1,3 +1,4 @@
 #include "CPUInfo.hpp"
 
 extern int cpuid_check(void);
+extern void cpuid_get(int op, int *eax, int *ebx, int *ecx, int *edx);
--- src/core/hardware/cpu_spec.asm
+++ src/core/hardware/cpu_spec.asm
@@ -2,8 +2,6 @@
 
 SECTION .TEXT
 
-GLOBAL cpuid_check
-
 ;------------------------------
 ; int cpuid_check(void)
 ; return 0 if unsupported.
@@ -28,5 +26,37 @@ cpuid_check:
 	pop		ebp
 	pop		ebx
 	popfd
+	ret
+	
+;
+; void cpuid_get(int op, int *eax, int *ebx, int *ecx, int *edx)
+;
+cpuid_get:
+	push 	ebp			; Creating the stackframe.
+	mov 	ebp, esp
+	push	eax
+	push	ebx
+	push	ecx
+	push	edx
+	push 	esi
+	push	edi
+	
+	; Moving the op variable into eax register before calling cpuid instruction.
+	mov		eax, dword [ebp + 8]
+	cpuid
+	
+	mov		dword [ebp + 12], eax
+	mov		dword [ebp + 10h], ebx
+	mov		dword [ebp + 14h], ecx
+	mov		dword [ebp + 18h], edx
+	
+	pop		edi			; Deleting the stack.
+	pop		esi
+	pop		edx
+	pop		ecx
+	pop		ebx
+	pop		edx
+	mov 	esp, ebp
+	pop 	ebp
 	ret
 	
